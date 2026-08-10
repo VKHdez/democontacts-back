9205fb6d22b7cf269f1876f42e9ff8dc386e7710
Victor Hdez <28125420+VKHdez@users.noreply.github.com>
2026-08-09 22:21:56 -0600

    ADD :: Base controller for Persona

---
+++ src/main/java/com/vktechnologies/democontact/presentation/rest/v1/controllers/PersonaController.java
@@ -0,0 +1,17 @@
+package com.vktechnologies.democontact.presentation.rest.v1.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vktechnologies.democontact.presentation.rest.ApiPaths;
+
+@RestController
+@RequestMapping(ApiPaths.v1 + "/persons")
+public class PersonaController {
+
+	public void update()
+	{}
+	
+	public void delete()
+	{}
+}
